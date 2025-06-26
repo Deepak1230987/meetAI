6f6e1d9c2399b579bc16bd12e0921f18888f2bdd
Deepak1230987 <singhdeepak@kgpian.iitkgp.ac.in>
2025-06-26 05:54:08 +0530

    feat(agents): implement agent creation and retrieval with protected routes and forms

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -51,7 +51,7 @@ export const agents = pgTable("agents", {
 	id: text('id').primaryKey().$defaultFn(() => nanoid()),
 	name: text('name').notNull(),
 	userId: text("user_id").notNull().references(() => user.id, { onDelete: 'cascade' }),
-	innstructions: text('instructions').notNull(),
+	instructions: text('instructions').notNull(),
 	createdAt: timestamp('created_at').notNull().defaultNow(),
 	updatedAt: timestamp('updated_at').notNull().defaultNow()
 
