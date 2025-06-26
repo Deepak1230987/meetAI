e02bfb5e49b7f2e4c2614b20ec5ee8104322b04a
Deepak1230987 <singhdeepak@kgpian.iitkgp.ac.in>
2025-06-26 10:21:56 +0530

    feat(agents): add confirmation dialog and update agent functionality in agent view

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -2,7 +2,7 @@ import { db } from "@/db";
 import { agents } from "@/db/schema";
 import { and, desc, eq, getTableColumns, ilike, sql, count } from "drizzle-orm";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { agentsInsertSchema } from "../schema";
+import { agentsInsertSchema, agentUpdateSchema } from "../schema";
 import { z } from "zod";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
@@ -10,22 +10,61 @@ import { TRPCError } from "@trpc/server";
 
 
 export const agentsRouter = createTRPCRouter({
-    // todo: change "getone" to use "protectedprocedure"
+    update: protectedProcedure
+        .input(agentUpdateSchema)
+        .mutation(async ({ ctx, input }) => {
+            const [updatedAgent] = await db
+                .update(agents)
+                .set(input)
+                .where(
+                    and(
+                        eq(agents.id, input.id),
+                        eq(agents.userId, ctx.auth.user.id),
+                    )
+                ).returning();
+            if (!updatedAgent) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Agent not found"
+                })
+            }
+            return updatedAgent;
+        }),
+
+    remove: protectedProcedure.input(z.object({ id: z.string() })).mutation(async ({ ctx, input }) => {
+        const [removedAgent] = await db
+            .delete(agents)
+            .where(
+                and(
+                    eq(agents.id, input.id),
+                    eq(agents.userId, ctx.auth.user.id),
+                )
+            )
+            .returning();
+
+        if (!removedAgent) {
+            throw new TRPCError({
+                code: "NOT_FOUND",
+                message: "Agent not found"
+            })
+
+        }
+        return removedAgent;
+    }),
+
     getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input, ctx }) => {
         const [existingAgents] = await db.select({
             //todo: change to actual count
             meetingCount: sql<number>`5`,
             ...getTableColumns(agents),
 
-        }).from(agents).where(and(eq(agents.id, input.id),
-            eq(agents.userId, ctx.auth.user.id) 
-        ));
-if(!existingAgents) {
-    throw new TRPCError({
-        code: "NOT_FOUND",
-        message: "Agent not found",
-    })
-}
+        }).from(agents).where(and(eq(agents.id, input.id), eq(agents.userId, ctx.auth.user.id)));
+        if (!existingAgents) {
+            throw new TRPCError({
+                code: "NOT_FOUND",
+                message: "Agent not found",
+            })
+        }
 
         return existingAgents;
     }),
